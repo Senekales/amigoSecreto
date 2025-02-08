ca551a840c28690b6802eec9856e897f3113f7ec
Senekales <rodolfo.fuentes.a@gmail.com>
2025-02-07 22:48:57 -0300

    Se eliminan funciones y se agrega validación de lista en html y array

---
+++ app2.js
@@ -0,0 +1,52 @@
+let amigos = [];
+
+function agregarAmigo(){
+    let inputElement= document.getElementById('amigo');
+    let valor = inputElement.value.trim();
+
+    if(valor ===''){
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+
+    agregaAmigoAlArreglo(valor);    
+    actualizarListaAmigosHTML();
+    inputElement.value ='';
+}
+
+function agregaAmigoAlArreglo(nombre){  
+    console.log(`agregando amigo: ${nombre}`)
+    amigos.push(nombre); // Guarda el nombre tal como se ingresó
+    console.log(`la lista de amigos ahora es ${amigos}`);
+}
+
+function borrarNombresRepetidos(amigos){
+    const nombresEnMinuscula = amigos.map(nombre => nombre.toLowerCase());
+    let conjuntoUnicoMinusculas = new Set(nombresEnMinuscula);
+
+    const conjuntoUnicoOriginalCase = new Set();
+    nombresEnMinuscula.forEach(nombreMinuscula => {
+        const nombreOriginal = amigos.find(amigo => amigo.toLowerCase() === nombreMinuscula);
+        conjuntoUnicoOriginalCase.add(nombreOriginal);
+    });
+    return conjuntoUnicoOriginalCase;
+}
+
+
+function actualizarListaAmigosHTML() {
+    let listaAmigosElement = document.getElementById('listaAmigos');
+
+    if (!listaAmigosElement) {
+        console.error("Error: No se encontró el elemento con ID 'listaAmigos' en el HTML.");
+        return;
+    }
+
+    listaAmigosElement.innerHTML = '';
+    const nombresUnicos = borrarNombresRepetidos(amigos);
+
+    nombresUnicos.forEach(nombreUnico => {
+        let nuevoAmigo = document.createElement('li');
+        nuevoAmigo.textContent = nombreUnico;
+        listaAmigosElement.appendChild(nuevoAmigo);
+    });
+}
